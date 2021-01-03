da7915acee2fde078590cc111c4193bcc400ccad
Kostas Xanthopoulos <konxantho@gmail.com>
2021-01-03 15:15:55 +0200

    catch connection error in get request

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from requests import get
+import requests
 
 
 def get_input_text_from_url(day):
@@ -12,10 +12,20 @@ def get_input_text_from_url(day):
     session_cookie = '53616c7465645f5f6104714e96b2eff50399bd7e83c39c81a77c07927958a02bd0ae33e547765235a01e7d3fed18d1ec'
     cookies = {'session': session_cookie}
 
-    resp = get(f'https://adventofcode.com/2020/day/{day}/input', cookies=cookies)
+    try:
+        resp = requests.get(f'https://adventofcode.com/2020/day/{day}/input',
+                            cookies=cookies)
+    except requests.exceptions.ConnectionError:
+        print('[ERROR] Could not connect to url')
+        raise ConnectionError
 
     if resp.status_code == 400:
         print(f'[ERROR] {resp.text}')
         raise ValueError('Invalid session cookie')
 
     return resp.text[:-1]  # remove the last \n
+
+
+def get_example_input():
+    with open('temp.txt', 'r') as f:
+        return f.read()
