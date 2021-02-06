60034be572c1fa81c491d753a6c912fd7da09b18
Kostas Xanthopoulos <konxantho@gmail.com>
2021-02-06 18:35:21 +0200

    day 20 part 2

--- day20.py
+++ day20.py
@@ -2,12 +2,14 @@ import numpy as np
 from math import prod
 from collections import defaultdict, Counter
 
-from utils import get_input_text, get_example_input_text
+from utils import get_input_text
+
+MONSTER_PATTERN = ('                  # ', '#    ##    ##    ###',
+                   ' #  #  #  #  #  #   ')
 
 
 def get_input_list():
     input_text = get_input_text(20)
-    # input_text = get_example_input_text()
 
     full_tiles = input_text.split('\n\n')
     tiles = {}
@@ -21,16 +23,12 @@ def get_input_list():
 
 def solve1(tiles: dict):
     tile_edges = _get_tile_edges(tiles)
-    # get lonely edges (do not belong to more than 1 tile)
-    lonely_edges = {x: v for x, v in tile_edges.items() if len(v) == 1}
-    lonely_tile_ids = [v[0] for k, v in lonely_edges.items()]
-    ltc = Counter(lonely_tile_ids)
-    # corner tiles must have 4 lonely edges (2 normal and 2 reversed)
-    corner_tile_ids = [key for key in ltc if ltc[key] == 4]
+    corner_tile_ids = _get_corner_ids(tile_edges)
     return prod(corner_tile_ids)
 
 
 def _get_tile_edges(tiles: dict):
+    """Creates a dictionary that maps any edges and its reversed to a tile id."""
     tile_edges = defaultdict(list)
     for tile_id, tile in tiles.items():
         edge1_str = ''.join([x for x in tile[0, :]])  # first row
@@ -53,59 +51,122 @@ def _get_tile_edges(tiles: dict):
     return tile_edges
 
 
+def _get_corner_ids(tile_edges: dict):
+    """Gets the ids of the corner edges."""
+    # lonely_edges = {x: v for x, v in tile_edges.items() if len(v) == 1}
+    lonely_tile_ids = [v[0] for k, v in tile_edges.items() if len(v) == 1]
+    ltc = Counter(lonely_tile_ids)
+    # corner tiles must have 4 lonely edges (2 normal and 2 reversed)
+    return [key for key in ltc if ltc[key] == 4]
+
+
 def solve2(tiles: dict):
-    not_used_tiles = list(tiles.keys())
-    rows = []
-    # row_ids = []
-    r = 0
-    while not_used_tiles:
-        if r == 2:
-            break
-        row, not_used_tiles = _get_filled_row(tiles, not_used_tiles)
-        # print(not_used_tiles)
-        rows.append(row)
-        # print(row.shape)
-        if r == 1:
-            _print(row)
-        print(row.shape)
-        print('===')
-        # print('adssad')
-        r += 1
+    tile_edges = _get_tile_edges(tiles)
+    corner_ids = _get_corner_ids(tile_edges)
 
+    number_of_rows = int(len(tiles)**0.5)
+    np_image = None
 
-def _get_filled_row(tiles, not_used_tiles):
-    row = tiles[not_used_tiles[0]]
-    print(not_used_tiles[0])
-    not_used_tiles = not_used_tiles[1:]  # remove first tile
-    row, not_used_tiles, row_ids = _fill_to_the_right(tiles, not_used_tiles, row)
-    # row = np.flip(row, 1)
-    # print('flip')
-    # row, not_used_tiles, row_ids2 = _fill_to_the_right(tiles, not_used_tiles, row)
-    # print(row_ids + row_ids2)
-    return row, not_used_tiles
+    first_of_row_id = corner_ids[0]
+    first_of_row = tiles[first_of_row_id]
 
+    tiles_used = set()
+    for _ in range(number_of_rows):
+        # intialize row
+        row = first_of_row
+        tiles_used.add(first_of_row_id)
 
-def _fill_to_the_right(tiles, not_used_tiles, row):
-    w = np.array(['|'] * 10)
-    w = np.reshape(w, (10, 1))
-    row_ids = []
-    while True:
-        for tile_id in not_used_tiles:
-            check_tile = tiles[tile_id]
-            neighbor = _get_right_neighbor(row, check_tile)
-            if neighbor is not None:
-                # if tile_id == 1847:
-                # _print(row)
-                # print('das---das')
-                # _print(neighbor)
-                # row = np.concatenate((row, w), axis=1)
+        while True:
+            right_edge_of_row = ''.join(list(row[:, -1]))
+            connected_to_right_edge = tile_edges[right_edge_of_row]
+            try:
+                neighbor_id = [
+                    x for x in connected_to_right_edge if x not in tiles_used
+                ][0]
+                neighbor = tiles[neighbor_id]
+                neighbor = _get_right_neighbor(row, neighbor)
+                # update row to the right
                 row = np.concatenate((row, neighbor), axis=1)
-                row_ids.append(tile_id)
-                not_used_tiles.remove(tile_id)
+                tiles_used.add(neighbor_id)
+            except IndexError:
+                # row done
                 break
+
+        # update image with the filled row
+        if np_image is None:
+            np_image = row
         else:
+            np_image = np.concatenate((np_image, row), axis=0)
+
+        # find the bottom neighbor of the first tile in the row
+        bot_edge_of_first = ''.join(list(first_of_row[-1, :]))
+        connected_to_first = tile_edges[bot_edge_of_first]
+        try:
+            bot_neighbor = [x for x in connected_to_first
+                            if x != first_of_row_id][0]
+        except IndexError:
+            # no other bot neighbors. All rows done
             break
-    return row, not_used_tiles, row_ids
+        # update the details of the first tile of the next row
+        first_of_row_id = bot_neighbor
+        first_of_row = _get_down_neighbor(first_of_row, tiles[bot_neighbor])
+
+    np_image = _remove_tile_borders(np_image)
+    str_image = '\n'.join([''.join(list(x)) for x in np_image])
+    number_of_monsters = _count_pattern(np_image, MONSTER_PATTERN)
+
+    water_in_image = str_image.count('#')
+    water_in_the_monster = sum([x.count('#') for x in MONSTER_PATTERN])
+    water_in_all_monsters = water_in_the_monster * number_of_monsters
+    return water_in_image - water_in_all_monsters
+
+
+def _remove_tile_borders(image: np.array):
+    """Removes the boreders of the tiles that compose the image."""
+    number_of_rows = image.shape[0] // 10
+    ind_to_delete = []
+    for i in range(number_of_rows):
+        # first(0) and last(9) col/row of each tile
+        ind_to_delete.extend([i * 10, i * 10 + 9])
+
+    for i, ind in enumerate(ind_to_delete):
+        # -i: the rows/cols that have already been deleted
+        image = np.delete(image, ind - i, 1)
+        image = np.delete(image, ind - i, 0)
+    return image
+
+
+def _count_pattern(image, pattern):
+    pattern_h, pattern_w = len(pattern), len(pattern[0])
+    image_sz = image.shape[0]
+    deltas = set()
+
+    for r, row in enumerate(pattern):
+        for c, cell in enumerate(row):
+            if cell == '#':
+                deltas.add((r, c))
+
+    for img in _arrangements(image):
+        n = 0
+        for r in range(image_sz - pattern_h):
+            for c in range(image_sz - pattern_w):
+                if all(img[r + dr][c + dc] == '#' for dr, dc in deltas):
+                    n += 1
+
+        if n != 0:
+            return n
+
+
+def _arrangements(tile):
+    yield from _orientations(tile)
+    yield from _orientations(np.flip(tile, 0))
+
+
+def _orientations(tile):
+    yield tile
+    for _ in range(3):
+        tile = np.rot90(tile, 1)
+        yield tile
 
 
 def _get_right_neighbor(tile1: np.array, tile2: np.array):
@@ -126,32 +187,53 @@ def _get_right_neighbor(tile1: np.array, tile2: np.array):
     elif tile1_edge == tile2_top_edge:
         return np.flip(np.rot90(tile2, 1), 0)
     elif tile1_edge == tile2_top_edge[::-1]:
-        return np.rot90(tile2, 0)
+        return np.rot90(tile2, 1)
     elif tile1_edge == tile2_bot_edge:
-        return np.rot90(tile2, 3)
+        return np.rot90(tile2, -1)
     elif tile1_edge == tile2_bot_edge[::-1]:
-        return np.flip(np.rot90(tile2, 3), 0)
+        return np.flip(np.rot90(tile2, -1), 0)
     else:
         return
 
 
-def _print(tile):
-    str_rows = [''.join(list(x)) for x in tile]
-    print('\n'.join(str_rows))
+def _get_down_neighbor(tile1: np.array, tile2: np.array):
+    tile1_edge = list(tile1[-1, :])
+    tile2_left_edge = list(tile2[:, 0])
+    tile2_right_edge = list(tile2[:, -1])
+    tile2_top_edge = list(tile2[0, :])
+    tile2_bot_edge = list(tile2[-1, :])
+
+    if tile1_edge == tile2_top_edge:
+        return tile2
+    elif tile1_edge == tile2_top_edge[::-1]:
+        return np.flip(tile2, 1)  # flip in y axis
+    elif tile1_edge == tile2_bot_edge:
+        return np.flip(tile2, 0)  # flip in x axis
+    elif tile1_edge == tile2_bot_edge[::-1]:
+        return np.flip(tile2, (0, 1))  # flip in both axis
+    elif tile1_edge == tile2_right_edge:
+        return np.rot90(tile2, 1)
+    elif tile1_edge == tile2_right_edge[::-1]:
+        return np.flip(np.rot90(tile2, 1), 1)
+    elif tile1_edge == tile2_left_edge:
+        return np.flip(np.rot90(tile2, -1), 1)
+    elif tile1_edge == tile2_left_edge[::-1]:
+        return np.rot90(tile2, -1)
+    else:
+        return
 
 
 def solve():
     tiles = get_input_list()
 
     # PART 1
-    # multiplied_edges = solve1(tiles)
-    # print(f'[PART 1] The multiplied edges are {multiplied_edges}')
+    multiplied_edges = solve1(tiles)
+    print(f'[PART 1] The multiplied edges are {multiplied_edges}')
 
     # PART 2
     non_sea_monster = solve2(tiles)
-    # print(f'[PART 2] {non_sea_monster} # are not part of a monster')
+    print(f'[PART 2] {non_sea_monster} # are not part of a monster')
 
 
 if __name__ == '__main__':
     solve()
-    pass
